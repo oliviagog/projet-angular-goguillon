6195366b7c1e8734d9c27a76efda8ee5c0d23852
oliviagog <olivia.goguillon62@gmail.com>
2021-04-20 17:36:09 +0200

    formulaire ajouter fabrication

--- src/app/fabrication/pages/fabrication-form/fabrication-form.component.ts
+++ src/app/fabrication/pages/fabrication-form/fabrication-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Fabrication } from 'src/app/core/models/fabrication';
+import { FabricationService } from 'src/app/core/services/http/fabrication.service';
 
 @Component({
   selector: 'app-fabrication-form',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FabricationFormComponent implements OnInit {
 
-  constructor() { }
+  fabricationForm: FormGroup;
+ 
+
+  constructor(private fb: FormBuilder, private _fabricationService: FabricationService, private _route: Router) {
+    this.fabricationForm = this.fb.group({
+      createur: ['', [Validators.required, this.noWhitespaceValidator]],
+      prix: ['', [Validators.required, this.noWhitespaceValidator]],
+      chocolatid: ['', [Validators.required, this.noWhitespaceValidator]],
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  public noWhitespaceValidator(control: FormControl) {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const isValid = !isWhitespace;
+    return isValid ? null : { 'whitespace': true };
+  }
+
+  onSubmit(fabrication: Fabrication) {
+    if (this.fabricationForm.valid) {
+      this._fabricationService.post(fabrication).subscribe(next => {
+        console.log("Fabrication:", next);
+        this.fabricationForm.reset();
+        this._route.navigateByUrl("/fabrication");
+      });
+    }
   }
 
 }
